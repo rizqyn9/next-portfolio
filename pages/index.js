5e3b335c6a73a56e8ea6d78458fbf752aa5a1b39
rizqy <rizqynugroho9@gmail.com>
2021-07-11 02:33:00 +0700

    heheh banyak error

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Nav from '../components/nav/Nav'
-import {Switch,BrowserRouter, Route} from 'react-router-dom'
 import Home from '../components/home/home'
 import {motion} from 'framer-motion'
 // import LoadingPage from '../components/loading/LoadingPage'
